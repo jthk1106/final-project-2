546d28ed7d78c1fe871e9e6f82587a84691590cf
Jeremy Kim <thk116@gmail.com>
2018-05-11 09:34:50 +0000

    added service and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ClarityModule } from "@clr/angular";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { StockService } from './stock.service';
+import { MainComponent } from './main/main.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ClarityModule, HttpClientModule
   ],
-  providers: [],
+  providers: [
+    StockService  
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/stock.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class StockService {
+    //https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&outputsize=compact&apikey=demo
+    url: "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="
+    symbol: "MSFT"
+    end: "&outputsize=compact&apikey=Y1UPJCU22ZH52Z7Z"
+  constructor(private http: HttpClient) {
+      
+      getData(){
+          return this.http.get(this.url+this.symbol+this.end)
+      }
+  }
+
+}
